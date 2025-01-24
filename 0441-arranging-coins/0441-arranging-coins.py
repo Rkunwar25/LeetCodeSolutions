8e8dfe39fbff84fbbf6ba64854cab7c6dcc1921d
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-01-24 21:17:31 +0530

    Time: 3 ms (71.08%), Space: 12.3 MB (52.51%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def arrangeCoins(self, n):
-        rc=0
-        i=1
-        while n>=0:
-            n-=i
-            rc+=1
-            i+=1
-        return rc-1
+        # rc=0
+        # i=1
+        # while n>=0:
+        #     n-=i
+        #     rc+=1
+        #     i+=1
+        # return rc-1
+        return int(((-1 + (1 + 8 * n) ** 0.5) // 2))
