a2a53c371338ac89d56d0b393513f9c08214e5d3
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-02-01 13:29:21 +0530

    Time: 0 ms (100%), Space: 12.4 MB (62.31%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def uniqueOccurrences(self, arr):
         count=Counter(arr)
-        if len(count.values())==len(set(count.values())):
-            return True
-        return False         
+        return len(count.values())==len(set(count.values()))
+            
