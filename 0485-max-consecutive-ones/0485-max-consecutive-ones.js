1966f58b6e1d9b6f2b33bddb27b3a1a7a609607c
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-02-14 20:20:35 +0530

    Time: 9 ms (5.99%), Space: 56.4 MB (5.05%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -5,8 +5,14 @@
 var findMaxConsecutiveOnes = function(nums) {
     if (!nums.includes(1)) return 0; // Edge case: No 1s in the array
 
-    let s = nums.join('');
-    let x = s.split(/[^1]/); // Split by non-1 characters
+    var s = nums.join('');
+    var x = s.split(/[^1]/); // Split by non-1 characters
+    var maxsize=x[0].length;
+    for (let i=1;i<x.length;i++)
+    {
+        if(x[i].length>maxsize)
+          maxsize=x[i].length
+    }
+    return maxsize
 
-    return Math.max(...x.map(str => str.length)); // Get the longest sequence length
 };
