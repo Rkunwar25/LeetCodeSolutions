cf37a99b5b7d525ebe673214b4eaaa6182a25e0d
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-03-01 20:20:01 +0530

    Time: 0 ms (100%), Space: 12.6 MB (55.6%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
@@ -1,8 +1,20 @@
+# class Solution(object):
+#     def maximumDifference(self, nums):
+#         maxd=-1
+#         for i in range(0,len(nums)-1):
+#           for j in range(i+1,len(nums)) :
+#             if (nums[j]-nums[i])>maxd and (nums[j]-nums[i])>0:
+#                 maxd=nums[j]-nums[i]
+#         return maxd
 class Solution(object):
     def maximumDifference(self, nums):
-        maxd=-1
-        for i in range(0,len(nums)-1):
-          for j in range(i+1,len(nums)) :
-            if (nums[j]-nums[i])>maxd and (nums[j]-nums[i])>0:
-                maxd=nums[j]-nums[i]
+        min_val = nums[0]
+        maxd = -1
+        
+        for i in range(1, len(nums)):
+            if nums[i] > min_val:
+                maxd = max(maxd, nums[i] - min_val)
+            else:
+                min_val = nums[i]  # Update min_val when a new smaller element is found
+        
         return maxd
