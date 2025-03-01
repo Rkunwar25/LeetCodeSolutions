55e09d33e175985bee0a740cba1b6ed1e6fb6edb
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-03-01 20:11:30 +0530

    Time: 0 ms (100%), Space: 12.5 MB (53.53%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
@@ -1,14 +1,23 @@
+# class Solution(object):
+#     def finalValueAfterOperations(self, operations):
+#         X=0
+#         for i in operations:
+#             if i[0]=='X' and i[1]=='-' and i[2]=='-':
+#                 X-=1
+#             elif i[0]=='X' and i[1]=='+' and i[2]=='+':
+#                 X+=1
+#             elif i[0]=='-' and i[1]=='-' and i[2]=='X':
+#                 X-=1
+#             elif i[0]=='+' and i[1]=='+' and i[2]=='X':
+#                 X+=1
+            
+#         return X
 class Solution(object):
     def finalValueAfterOperations(self, operations):
-        X=0
+        X = 0
         for i in operations:
-            if i[0]=='X' and i[1]=='-' and i[2]=='-':
-                X-=1
-            elif i[0]=='X' and i[1]=='+' and i[2]=='+':
-                X+=1
-            elif i[0]=='-' and i[1]=='-' and i[2]=='X':
-                X-=1
-            elif i[0]=='+' and i[1]=='+' and i[2]=='X':
-                X+=1
-            
+            if i[1] == '-':
+                X -= 1
+            else:
+                X += 1
         return X
