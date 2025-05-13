e063373e15e39e20ced616bd262455569ab8b3f4
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-13 20:03:12 +0530

    Time: 19 ms (54.81%), Space: 12.7 MB (8.01%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        def is_self_dividing(n):
+            for digit in str(n):
+                if digit == '0' or n % int(digit) != 0:
+                    return False
+            return True
+        
+        return [i for i in range(left, right + 1) if is_self_dividing(i)]
