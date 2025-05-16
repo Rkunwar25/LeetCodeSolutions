ac01304b509eabbcd31fc7e4f69fbf70e35b2899
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-16 12:11:07 +0530

    Time: 24 ms (5.36%), Space: 12.4 MB (49.11%) - LeetHub

--- 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.py
+++ 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.py
@@ -1,17 +1,27 @@
 class Solution(object):
     def sortEvenOdd(self, nums):
-        l1=[nums[i] for i in range(len(nums)) if i%2==0]
-        l2=[nums[i] for i in range(len(nums)) if i%2!=0]
-        l1.sort()
-        l2.sort(reverse=True)
-        l=[]
-        k1=0
-        k2=0
-        for i in range(len(l1)+len(l2)):
-            if i%2==0:          
-                l.append(l1[k1])
-                k1+=1
-            else:
-                l.append(l2[k2])
-                k2+=1
-        return l
+        # l1=[nums[i] for i in range(len(nums)) if i%2==0]
+        # l2=[nums[i] for i in range(len(nums)) if i%2!=0]
+        # l1.sort()
+        # l2.sort(reverse=True)
+        # l=[]
+        # k1=0
+        # k2=0
+        # for i in range(len(l1)+len(l2)):
+        #     if i%2==0:          
+        #         l.append(l1[k1])
+        #         k1+=1
+        #     else:
+        #         l.append(l2[k2])
+        #         k2+=1
+        # return l
+        even = nums[::2]
+        odd = nums[1::2]
+
+        even.sort()
+        odd.sort(reverse=True)
+        
+        nums[::2] = even
+        nums[1::2] = odd
+        
+        return nums
