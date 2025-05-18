0703ebb7f37ef447ca64db3a16d1884f213c8ada
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-18 10:07:43 +0530

    Time: 0 ms (100%), Space: 12.5 MB (48.36%) - LeetHub

---
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def digitCount(self, num):
+        
+        for i in range(len(num)):
+            if num.count(str(i))!=int(num[i]):
+                return False
+        return True
