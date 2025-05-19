2b39b39deeaec32e16d02facd30410500b26dbaf
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-19 12:23:39 +0530

    Time: 0 ms (100%), Space: 12.5 MB (61.54%) - LeetHub

--- 1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.py
+++ 1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def numDifferentIntegers(self, word):
-        l=re.split(r"[a-z]",word)
-        st=list(set(l))
-        nl=[]
-        for i in st:
-            if (i!='') and (int(i) not in nl):
-                nl.append(int(i))
-        return len(nl)
+        # l=re.split(r"[a-z]",word)
+        # st=list(set(l))
+        # nl=[]
+        # for i in st:
+        #     if (i!='') and (int(i) not in nl):
+        #         nl.append(int(i))
+        # return len(nl)
+        numbers = set(map(int, re.findall(r"\d+", word)))
+        return len(numbers)
