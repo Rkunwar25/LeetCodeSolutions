cd018c16c647e90de785299a73ebe23f68fa9d49
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-19 12:50:12 +0530

    Time: 15 ms (64.47%), Space: 12.6 MB (14.78%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.py
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def sortSentence(self, s):
-        d=dict()
-        s=s.split(' ')
-        for i in s:
-              d[int(i[-1])]=i[:-1]
-        d=dict(sorted(d.items()))
-        return " ".join(d.values())
+        # d=dict()
+        # s=s.split(' ')
+        # for i in s:
+        #       d[int(i[-1])]=i[:-1]
+        # d=dict(sorted(d.items()))
+        # return " ".join(d.values())
+                return ' '.join(word[:-1] for word in sorted(s.split(), key=lambda x: int(x[-1])))
