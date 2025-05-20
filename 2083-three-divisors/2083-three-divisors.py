a2e5892139a43ed05de83f2074c873b235895024
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-20 10:51:52 +0530

    Time: 4 ms (52.02%), Space: 12.5 MB (70.55%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.py
+++ 2083-three-divisors/2083-three-divisors.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def isThree(self, n):
-        c=0
-        for i in range(1,n+1):
+        c=2
+        for i in range(2,n//2+1):
             if n%i==0:
                 c+=1
         return c==3
