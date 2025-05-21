4b0860229240a2fbba8f1e7e49737cde839b068f
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-21 20:05:17 +0530

    Time: 0 ms (100%), Space: 12.5 MB (12.7%) - LeetHub

--- 2668-form-smallest-number-from-two-digit-arrays/2668-form-smallest-number-from-two-digit-arrays.py
+++ 2668-form-smallest-number-from-two-digit-arrays/2668-form-smallest-number-from-two-digit-arrays.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def minNumber(self, nums1, nums2):
-        l=list(set(nums1) & set(nums2))
+        l=set(nums1) & set(nums2)
         if l:
             return min(l)
         n=min(nums1)
         m=min(nums2)
-        if n>m:
-            return int(str(m)+str(n))
-        else:
-            return int(str(n)+str(m))
+        return min(n*10+m,m*10+n)
