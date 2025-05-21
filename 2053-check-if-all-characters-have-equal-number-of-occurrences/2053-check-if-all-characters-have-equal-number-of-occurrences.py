254430338af0e005f67d240232b532c0e6e23792
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-21 12:07:17 +0530

    Time: 7 ms (44.15%), Space: 12.5 MB (19.98%) - LeetHub

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def areOccurrencesEqual(self, s):
+        c=Counter(s)
+        l=[]
+        for i,j in c.items():
+            l.append(j)
+        return len(list(set(l)))==1
