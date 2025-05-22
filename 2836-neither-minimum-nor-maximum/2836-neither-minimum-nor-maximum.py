d5a70502bd4e937ddf99aa2ec55a47fbef37a81b
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-22 10:05:19 +0530

    Time: 27 ms (20.79%), Space: 12.6 MB (9.9%) - LeetHub

--- 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.py
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.py
@@ -1,8 +1,10 @@
 class Solution(object):
     def findNonMinOrMax(self, nums):
+        # nums.sort()
+        # if nums:
+        #           nums.pop(nums.index(max(nums)))
+        # if nums:
+        #           nums.pop(nums.index(min(nums)))
+        # return nums[0] if nums else -1
         nums.sort()
-        if nums:
-                  nums.pop(nums.index(max(nums)))
-        if nums:
-                  nums.pop(nums.index(min(nums)))
-        return nums[0] if nums else -1
+        return nums[1] if (nums and len(nums)>=3) else -1
