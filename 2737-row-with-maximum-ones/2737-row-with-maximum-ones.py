39a01cf5c2a99de3349ec811840ee67f4e89723b
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-24 09:06:02 +0530

    Time: 14 ms (37.57%), Space: 12.7 MB (60.85%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
@@ -1,13 +1,21 @@
 class Solution(object):
     def rowAndMaximumOnes(self, mat):
-        d=dict()
-        for i in range(len(mat)):
-            d[i]=mat[i].count(1)
-        mn=float('-inf')
-        for i,j in d.items():
-            mn=max(mn,j)
-        l=[]
-        for i in d.keys():
-            if d[i]==mn:
-                l.append(i)
-        return [min(l),mn]
+        # d=dict()
+        # for i in range(len(mat)):
+        #     d[i]=mat[i].count(1)
+        # mn=float('-inf')
+        # for i,j in d.items():
+        #     mn=max(mn,j)
+        # l=[]
+        # for i in d.keys():
+        #     if d[i]==mn:
+        #         l.append(i)
+        # return [min(l),mn]
+        max_count = -1
+        row_index = -1
+        for i, row in enumerate(mat):
+            count_ones = row.count(1)
+            if count_ones > max_count:
+                max_count = count_ones
+                row_index = i
+        return [row_index, max_count]
