b2aa1f5179855e8aa15d52b755d3282271e54252
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-05-25 20:13:00 +0530

    Time: 0 ms (100%), Space: 12.4 MB (62.07%) - LeetHub

--- 1363-greatest-english-letter-in-upper-and-lower-case/1363-greatest-english-letter-in-upper-and-lower-case.py
+++ 1363-greatest-english-letter-in-upper-and-lower-case/1363-greatest-english-letter-in-upper-and-lower-case.py
@@ -1,7 +1,12 @@
 class Solution(object):
     def greatestLetter(self, s):
-        greatest=0
-        for i in range(ord('A'),ord('Z')+1):
-            if (chr(i).lower() in s) and (chr(i) in s):
-                greatest=i
-        return chr(greatest) if greatest!=0 else ""
+        # greatest=0
+        # for i in range(ord('A'),ord('Z')+1):
+        #     if (chr(i).lower() in s) and (chr(i) in s):
+        #         greatest=i
+        # return chr(greatest) if greatest!=0 else ""
+        s1=set(s)
+        for i in range(ord('Z'),ord('A')-1,-1):
+            if (chr(i) in s1) and (chr(i).lower() in s1):
+                return chr(i)
+        return ""
