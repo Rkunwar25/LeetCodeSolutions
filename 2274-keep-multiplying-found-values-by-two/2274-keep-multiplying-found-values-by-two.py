e22cf2ffb421a6b9bce34fa44af476bc79144f76
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-06-05 09:57:28 +0530

    Time: 0 ms (100%), Space: 12.7 MB (11.76%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.py
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.py
@@ -1,15 +1,21 @@
+# class Solution(object):
+#     def findFinalValue(self, nums, original):
+#         def prod(nums,targ):
+#            for i in nums:
+#               if i==targ:
+#                 i=i*2
+#                 break
+#            return i     
+        
+#         while True:
+#             if original in nums:
+#                 original=prod(nums,original)
+#             else:
+#                 break          
+#         return original
 class Solution(object):
     def findFinalValue(self, nums, original):
-        def prod(nums,targ):
-           for i in nums:
-              if i==targ:
-                i=i*2
-                break
-           return i     
-        
-        while True:
-            if original in nums:
-                original=prod(nums,original)
-            else:
-                break          
+        num_set = set(nums)  # O(1) lookups
+        while original in num_set:
+            original *= 2
         return original
