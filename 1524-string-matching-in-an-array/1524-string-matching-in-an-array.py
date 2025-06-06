28b546c0e0cd03e771d59528fa74482c293a4e8d
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-06-06 09:27:22 +0530

    Time: 0 ms (100%), Space: 12.5 MB (10.65%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -6,5 +6,5 @@ class Solution(object):
             for j in range(n):
                 if i!=j and words[i] in words[j]:
                     l.append(words[i])
-                    continue
+                    break
         return list(set(l))
