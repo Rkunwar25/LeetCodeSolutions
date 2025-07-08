bc9b3b274d6a25d3bea73142a5022a494eb36910
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-07-08 12:04:24 +0530

    Time: 0 ms (100%), Space: 8.3 MB (28.06%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.c
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.c
@@ -1,35 +1,43 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
- #include<math.h>
-int getDecimalValue(struct ListNode* head) {
-    int c=0;
-    struct ListNode *temp=head;
-    while(temp)
-    {
-        c++;
-        temp=temp->next;
-    }
-    int *p=(int*)malloc(c*sizeof(int));
-    free(temp);
-    temp=head;
-    int i=0;
-    while(temp)
-    {
-        p[i++]=temp->val;
-        temp=temp->next;
-    }
-    free(temp);
-    int pw=0,val=0;
-    for(i=c-1;i>=0;i--)
-    {
-        val+=pow(2,pw)*p[i];
-        pw++;   
-    } 
-    return val;
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     struct ListNode *next;
+//  * };
+//  */
+//  #include<math.h>
+// int getDecimalValue(struct ListNode* head) {
+//     int c=0;
+//     struct ListNode *temp=head;
+//     while(temp)
+//     {
+//         c++;
+//         temp=temp->next;
+//     }
+//     int *p=(int*)malloc(c*sizeof(int));
+//     free(temp);
+//     temp=head;
+//     int i=0;
+//     while(temp)
+//     {
+//         p[i++]=temp->val;
+//         temp=temp->next;
+//     }
+//     free(temp);
+//     int pw=0,val=0;
+//     for(i=c-1;i>=0;i--)
+//     {
+//         val+=pow(2,pw)*p[i];
+//         pw++;   
+//     } 
+//     return val;
 
+// }
+int getDecimalValue(struct ListNode* head) {
+    int val = 0;
+    while (head) {
+        val = val * 2 + head->val;
+        head = head->next;
+    }
+    return val;
 }
