d12aec236c09d9b49edd40d672118969ee53ce65
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-11-02 21:19:02 +0530

    Time: 3 ms (2.7%), Space: 42.9 MB (8.24%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -2,21 +2,31 @@ import java.util.HashMap;
 
 class Solution {
     public int sumOfUnique(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
         
-        // Count occurrences of each number
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // // Count occurrences of each number
+        // for (int num : nums) {
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+        
+        // int sum = 0;
+        // // Sum elements that occur exactly once
+        // for (int key : map.keySet()) {
+        //     if (map.get(key) == 1) {
+        //         sum += key;
+        //     }
+        // }
+
+        int sum=0;
+        ArrayList<Integer> nums1=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            nums1.add(nums[i]);
         }
-        
-        int sum = 0;
-        // Sum elements that occur exactly once
-        for (int key : map.keySet()) {
-            if (map.get(key) == 1) {
-                sum += key;
+        for(int i:nums1){
+            if (Collections.frequency(nums1,i)==1){
+                sum+=i;
             }
         }
-        
         return sum;
     }
 }
