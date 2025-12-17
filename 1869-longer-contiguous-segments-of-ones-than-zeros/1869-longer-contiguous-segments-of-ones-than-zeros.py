1e4738c082b836834585dd05ba125f7eb8bdc844
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-12-17 21:11:36 +0530

    Time: 0 ms (100%), Space: 12.3 MB (100%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
@@ -1,20 +1,36 @@
+# class Solution(object):
+#     def checkZeroOnes(self, s):
+#         s=list(s)
+#         seq1=0
+#         seq2=0
+#         val=0
+#         for i in s:            
+#             if int(i) == 1:
+#                 val+=1
+#                 seq1=max(seq1,val)
+#             else:
+#                 val=0
+#         val=0
+#         for i in s:            
+#             if int(i) == 0:
+#                 val+=1
+#                 seq2=max(seq2,val)
+#             else:
+#                 val=0
+#         return seq1>seq2
 class Solution(object):
     def checkZeroOnes(self, s):
-        s=list(s)
-        seq1=0
-        seq2=0
-        val=0
-        for i in s:            
-            if int(i) == 1:
-                val+=1
-                seq1=max(seq1,val)
+        max1 = max0 = 0
+        count1 = count0 = 0
+        
+        for ch in s:
+            if ch == '1':
+                count1 += 1
+                max1 = max(max1, count1)
+                count0 = 0
             else:
-                val=0
-        val=0
-        for i in s:            
-            if int(i) == 0:
-                val+=1
-                seq2=max(seq2,val)
-            else:
-                val=0
-        return seq1>seq2
+                count0 += 1
+                max0 = max(max0, count0)
+                count1 = 0
+        
+        return max1 > max0
