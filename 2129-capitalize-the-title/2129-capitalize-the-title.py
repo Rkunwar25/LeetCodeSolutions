b746a337a813d6e11b45754c1af0c8672ee8ff4d
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-12-17 21:49:29 +0530

    Time: 0 ms (100%), Space: 18 MB (7.4%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.py
+++ 2129-capitalize-the-title/2129-capitalize-the-title.py
@@ -1,11 +1,17 @@
+# class Solution:
+#     def capitalizeTitle(self, title: str) -> str:
+#         title=title.lower().split()
+#         s=[]
+#         for i in title:
+#             if len(i)>2:
+#                 s.append(i.capitalize())
+#             else:
+#                 s.append(i.lower())
+#         return " ".join(s)
 class Solution:
     def capitalizeTitle(self, title: str) -> str:
-        title=title.lower().split()
-        s=[]
-        for i in title:
-            if len(i)>2:
-                s.append(i.capitalize())
-            else:
-                s.append(i.lower())
-        return " ".join(s)
+        return " ".join(
+            word.capitalize() if len(word) > 2 else word.lower()
+            for word in title.lower().split()
+        )
             
