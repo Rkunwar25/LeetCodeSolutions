1a9842b34ee1c59a28fee87d3e7901a53cba414c
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-12-23 20:36:21 +0530

    Time: 2 ms (13.82%), Space: 17.5 MB (98.03%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.py
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.py
@@ -2,15 +2,10 @@ class Solution:
     def monotoneIncreasingDigits(self, n: int) -> int:
         digits = list(str(n))
         mark = len(digits)
-        
-        # Traverse from right to left
         for i in range(len(digits) - 1, 0, -1):
             if digits[i - 1] > digits[i]:
                 digits[i - 1] = str(int(digits[i - 1]) - 1)
                 mark = i
-        
-        # Set all digits after mark to 9
         for i in range(mark, len(digits)):
-            digits[i] = '9'
-        
+            digits[i] = '9'        
         return int("".join(digits))
