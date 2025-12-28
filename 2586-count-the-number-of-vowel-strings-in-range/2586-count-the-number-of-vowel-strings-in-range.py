7e0971744b7d5b828a4fdd1a55224e7ec6f47242
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-12-28 12:00:05 +0530

    Time: 0 ms (100%), Space: 17.3 MB (97.39%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
@@ -1,7 +1,12 @@
+from typing import List
+
 class Solution:
     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-      count=0
-      for i in range(left,right+1):
-        if words[i][0] in 'aeiouAEIOU' and words[i][-1] in 'aeiouAEIUO':
-            count+=1
-      return count
+        vowels = set('aeiouAEIOU')
+        count = 0
+
+        for i in range(left, right + 1):
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                count += 1
+
+        return count
