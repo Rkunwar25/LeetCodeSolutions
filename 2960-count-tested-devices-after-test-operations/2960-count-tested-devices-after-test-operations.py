57650ac247cf6d06c46f15878e4ccebfa3d722a0
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-12-28 12:16:13 +0530

    Time: 0 ms (100%), Space: 17.2 MB (97.88%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        tested = 0
+
+        for battery in batteryPercentages:
+            if battery - tested > 0:
+                tested += 1
+
+        return tested
