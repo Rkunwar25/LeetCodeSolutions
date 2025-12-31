6f56ed419521cb6f2be575a507cf42c729d4f7a8
RAJ  KUNWAR <rkunwar473@gmail.com>
2025-12-31 20:04:22 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.95%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canMeasureWater(self, x: int, y: int, target: int) -> bool:
+        if target > x + y:
+            return False
+        if target == 0:
+            return True
+        
+        import math
+        return target % math.gcd(x, y) == 0
