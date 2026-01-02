86bb9077a75f7d8c86b8aa4849c7c3f87843a9df
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-02 10:52:06 +0530

    Time: 630 ms (23.58%), Space: 156.8 MB (98.11%) - LeetHub

--- 3527-find-the-most-common-response/3527-find-the-most-common-response.py
+++ 3527-find-the-most-common-response/3527-find-the-most-common-response.py
@@ -1,12 +1,7 @@
-from collections import Counter
-from typing import List
-
 class Solution:
     def findCommonResponse(self, responses: List[List[str]]) -> str:
         freq = Counter()
         
         for resp in responses:
             freq.update(set(resp))
-        
-        # Sort lexicographically first, then pick max by frequency
         return max(sorted(freq.keys()), key=lambda w: freq[w])
