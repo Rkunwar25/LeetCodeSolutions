ef68fc1879389221c5ee0bf77f31bafe48baf323
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-02 10:03:13 +0530

    Time: 4 ms (38.68%), Space: 17.7 MB (70.2%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,7 +1,17 @@
+# class Solution:
+#     def divideArray(self, nums: List[int]) -> bool:
+#         nset=set(nums)
+#         for i in nset:
+#             if nums.count(i)%2!=0:
+#                 return False
+#         return True
+from collections import Counter
+from typing import List
+
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        nset=set(nums)
-        for i in nset:
-            if nums.count(i)%2!=0:
+        freq = Counter(nums)
+        for v in freq.values():
+            if v % 2 != 0:
                 return False
         return True
