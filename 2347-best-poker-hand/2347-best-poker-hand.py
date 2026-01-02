c569a85aed99775ac84b2d8df680be015e491f0f
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-02 10:14:23 +0530

    Time: 2 ms (7.84%), Space: 17.4 MB (87.84%) - LeetHub

---
+++ 2347-best-poker-hand/2347-best-poker-hand.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+        # Check Flush
+        if len(set(suits)) == 1:
+            return "Flush"
+        
+        rank_freq = Counter(ranks)
+        max_count = max(rank_freq.values())
+        
+        # Check Three of a Kind
+        if max_count >= 3:
+            return "Three of a Kind"
+        
+        # Check Pair
+        if max_count >= 2:
+            return "Pair"
+        
+        return "High Card"
