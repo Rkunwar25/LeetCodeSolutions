f56e5d8457cc00bed1bc7a7974d117bc84751826
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-08 10:49:38 +0530

    Time: 0 ms (100%), Space: 12.6 MB (12.36%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def hammingDistance(self, x, y):
-        x=bin(x)[2:]
-        y=bin(y)[2:]
-        if len(x)<len(y):
-            x='0'*(len(y)-len(x))+x
-        elif len(x)>len(y):
-            y='0'*(len(x)-len(y))+y
-        hd=0
-        for i in range(len(x)):
-            if x[i]!=y[i]:
-                hd+=1
-        return hd
+        # x=bin(x)[2:]
+        # y=bin(y)[2:]
+        # if len(x)<len(y):
+        #     x='0'*(len(y)-len(x))+x
+        # elif len(x)>len(y):
+        #     y='0'*(len(x)-len(y))+y
+        # hd=0
+        # for i in range(len(x)):
+        #     if x[i]!=y[i]:
+        #         hd+=1
+        # return hd
+                return bin(x ^ y).count('1')
