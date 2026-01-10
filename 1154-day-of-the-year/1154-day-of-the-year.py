e58b0d8797bf782a06ce1c425e1dc8f01817fb21
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-10 10:45:04 +0530

    Time: 12 ms (50.27%), Space: 19.3 MB (6.32%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        year, month, day = map(int, date.split('-'))
+
+        days_in_month = [31, 28, 31, 30, 31, 30,
+                         31, 31, 30, 31, 30, 31]
+
+        # check leap year
+        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+            days_in_month[1] = 29
+
+        return sum(days_in_month[:month - 1]) + day
