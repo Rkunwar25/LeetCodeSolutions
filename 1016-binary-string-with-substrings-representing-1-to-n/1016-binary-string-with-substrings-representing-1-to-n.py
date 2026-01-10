c06e0ef2f5b9d5e16d83d56c7dffcf3bcbdb53c6
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-10 10:18:55 +0530

    Time: 0 ms (100%), Space: 19.4 MB (7.69%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.py
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.py
@@ -1,6 +1,9 @@
 class Solution:
     def queryString(self, s: str, n: int) -> bool:
-        for i in range(1,n+1):
-            if bin(i)[2:] not in s:
+        for i in range(1, n + 1):
+            b = bin(i)[2:]
+            if len(b) > len(s):
+                return False
+            if b not in s:
                 return False
         return True
