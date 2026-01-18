9aa15c8064387e0a7f9c8edc0f282c66df3ed309
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-18 10:38:13 +0530

    Time: 331 ms (41.13%), Space: 19.4 MB (8.37%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -2,8 +2,5 @@ class Solution:
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
         box = defaultdict(int)
         for i in range(lowLimit,highLimit+1):
-            s=list(str(i))
-            s1=sum(map(int,s))
-            box[s1]+=1
-        # print(box)
+            box[sum(map(int,list(str(i))))]+=1
         return max(box.values())
