3e2b478f74dae3501e9356ebb94ed2cae912ace9
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-24 10:33:39 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.72%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        l=[]
+        n=head
+        while n:
+            l.append(n.val)
+            n=n.next
+        newl=[]
+        some=[]
+        for i in l:
+            if i<x:
+                newl.append(i)
+            else:
+                some.append(i)
+        newl.extend(some)
+        new=None
+        tail=None
+        for i in newl:
+            n=ListNode(i)
+            if new is None:
+                new=n
+                tail=n
+            else:
+                tail.next=n
+                tail=n
+        return new
