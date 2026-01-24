b5225f7c17a3c3761b39a252ccb452815c04bf44
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-01-24 10:35:20 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.72%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -1,31 +1,52 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+#         l=[]
+#         n=head
+#         while n:
+#             l.append(n.val)
+#             n=n.next
+#         newl=[]
+#         some=[]
+#         for i in l:
+#             if i<x:
+#                 newl.append(i)
+#             else:
+#                 some.append(i)
+#         newl.extend(some)
+#         new=None
+#         tail=None
+#         for i in newl:
+#             n=ListNode(i)
+#             if new is None:
+#                 new=n
+#                 tail=n
+#             else:
+#                 tail.next=n
+#                 tail=n
+#         return new
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        l=[]
-        n=head
-        while n:
-            l.append(n.val)
-            n=n.next
-        newl=[]
-        some=[]
-        for i in l:
-            if i<x:
-                newl.append(i)
+        before = ListNode(0)
+        after = ListNode(0)
+
+        b = before
+        a = after
+
+        while head:
+            if head.val < x:
+                b.next = head
+                b = b.next
             else:
-                some.append(i)
-        newl.extend(some)
-        new=None
-        tail=None
-        for i in newl:
-            n=ListNode(i)
-            if new is None:
-                new=n
-                tail=n
-            else:
-                tail.next=n
-                tail=n
-        return new
+                a.next = head
+                a = a.next
+            head = head.next
+
+        a.next = None          # Important to avoid cycle
+        b.next = after.next   # Join the two lists
+
+        return before.next
