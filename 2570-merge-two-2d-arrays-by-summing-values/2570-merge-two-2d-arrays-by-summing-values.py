27eee7b9b3043b47e3d87664561bbe75bb0b6cdb
RAJ  KUNWAR <rkunwar473@gmail.com>
2026-02-22 11:31:31 +0530

    Time: 0 ms (100%), Space: 19.5 MB (61.55%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
@@ -1,21 +1,35 @@
+# class Solution:
+#     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+#         d1=dict()
+#         d2=dict()
+#         s=set()
+#         for i in nums1:
+#             d1[i[0]]=i[1]
+#             s.add(i[0])
+#         for i in nums2:
+#             d2[i[0]]=i[1]
+#             s.add(i[0])
+#         s=sorted(list(s))
+#         ans=[]
+#         for i in s:
+#             if i in d1 and i in d2:
+#                 ans.append([i,d1[i]+d2[i]])
+#             elif i in d1 and i not in d2:
+#                 ans.append([i,d1[i]])
+#             elif i in d2 and i not in d1:
+#                 ans.append([i,d2[i]])
+#         return ans
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        d1=dict()
-        d2=dict()
-        s=set()
-        for i in nums1:
-            d1[i[0]]=i[1]
-            s.add(i[0])
-        for i in nums2:
-            d2[i[0]]=i[1]
-            s.add(i[0])
-        s=sorted(list(s))
-        ans=[]
-        for i in s:
-            if i in d1 and i in d2:
-                ans.append([i,d1[i]+d2[i]])
-            elif i in d1 and i not in d2:
-                ans.append([i,d1[i]])
-            elif i in d2 and i not in d1:
-                ans.append([i,d2[i]])
-        return ans
+        d = {}
+        
+        for i, val in nums1:
+            d[i] = val
+            
+        for i, val in nums2:
+            if i in d:
+                d[i] += val
+            else:
+                d[i] = val
+                
+        return sorted(d.items())
